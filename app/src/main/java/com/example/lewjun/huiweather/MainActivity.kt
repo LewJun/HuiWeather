c08ea0c42b62c33dfa828f2ac9762ca89e13f5d2
LewJun <lewjun072@sina.com>
2018-04-10 22:54:31 +0800

    参数默认赋值

--- app/src/main/java/com/example/lewjun/huiweather/MainActivity.kt
+++ app/src/main/java/com/example/lewjun/huiweather/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.lewjun.huiweather
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tv_msg.text = "Hello Kotlin"
+        toast(tv_msg.text.toString())
+    }
+
+    private fun toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, message, duration).show()
     }
 }
